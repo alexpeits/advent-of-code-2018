d09d943a942b3bee677fe0cf921ff8c54f5ddeb1
Alexandros Peitsinis <alexpeitsinis@gmail.com>
2018-12-06 23:48:38 +0000

    Day 2 (1, 2)

---
+++ src/Advent/Day02.hs
@@ -0,0 +1,47 @@
+{-# LANGUAGE TupleSections #-}
+module Advent.Day02 where
+
+import Data.List (nub, tails)
+import qualified Data.Map as M
+
+import Advent.Util
+
+day :: Int
+day = 2
+
+counter :: Ord a => [a] -> M.Map a Int
+counter xs = M.fromListWith (+) $ zip xs (repeat 1)
+
+part1 :: [String] -> Int
+part1 =
+  product
+  . M.elems
+  . counter
+  . concatMap ( nub
+              . filter (`elem` [2, 3])
+              . M.elems
+              . counter
+              )
+
+combinations :: [a] -> [(a, a)]
+combinations = concatMap comb . init . tails
+  where comb (x:xs) = map (x,) xs
+
+differing :: Int -> String -> String -> Bool
+differing n x y = count > n
+  where
+    count = length $ filter (uncurry (/=)) $ zip x y
+
+part2 :: [String] -> String
+part2 =
+  map fst
+  . filter (uncurry (==))
+  . uncurry zip
+  . head
+  . dropWhile (uncurry (differing 1))
+  . combinations
+
+main :: IO ()
+main = do
+  input <- readInputLines day
+  printResults day (part1 input) (part2 input)
